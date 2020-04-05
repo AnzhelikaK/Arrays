c84b31020b78596b6569e2227c7744f1d79d7268
AnzhelikaK <AnzhelikaShch11@gmail.com>
2020-04-05 20:36:50 +0300

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("перевернуть массив чисел в обратном порядке");
+        System.out.println(Arrays.toString(Reverse.reverse2(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})));
+        int [] a=Reverse.reverse(null);
+
+        System.out.println("\n перевернуть строку в обратном порядке");
+       String b=Reverse.reverseString("Anzhelika");
+        System.out.println("Result: "+b+"\n");
+
+        System.out.println("сортировка массива чисел без использования Arrays.sort() - Cортировка Выбором");
+        System.out.println(Arrays.toString(Reverse.sort(new int[] {1,0,7,9,5,1,4,2,2,7,6,0,2,1,-5,-8,-97})));
+
+        System.out.println("\n сортировка массива чисел без использования Arrays.sort() - Cортировка Пузырьком");
+        System.out.println(Arrays.toString(Reverse.sort2(new int[] {1,0,7,9,5,1,4,2,2,7,6,0,2,1,-5,-8,-97})));
+    }
+}
---
+++ src/com/company/Reverse.java
@@ -0,0 +1,83 @@
+package com.company;
+
+import javax.crypto.NullCipher;
+
+public class Reverse {
+    // 1) перевернуть массив чисел в обратном порядке
+    public static int[] reverse(int[] array) {
+        if (array == null) {
+            System.out.println("Received empty array");
+            return new int[]{0};
+        } else {
+            int[] arrayTemporary = new int[array.length];
+            for (int i = 0; i < array.length; i++) {
+                arrayTemporary[i] = array.length - i;
+            }
+            return arrayTemporary;
+        }
+    }
+
+    //1) перевернуть массив чисел в обратном порядке: способ 2 - переворачивание без дополниетлнього массива
+    public static int[] reverse2(int[] array) {
+        if (array != null) {
+            int x = 0;
+            for (int i = 0; i < array.length / 2; i++) {
+                x = array[i];
+                array[i] = array[array.length - 1 - i];
+                array[array.length - 1 - i] = x;
+            }
+            return array;
+        } else {
+            return new int[]{0};
+        }
+    }
+
+
+    // 2) перевернуть строку в обратном порядке
+    public static String reverseString(String str) {
+        char[] charArray = str.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            stringBuilder.append(charArray[i]);
+        }
+        return stringBuilder.toString();
+    }
+
+    // 3) сортировка массива чисел без использования Arrays.sort(); Сортировка выбором
+    public static int[] sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int i_min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    i_min = j;
+                }
+            }
+            if (array[i] != min) {  // можно также по индеку сравнивать i!=i_min
+                array[i_min] = array[i];
+                array[i] = min;
+            }
+        }
+        return array;
+    }
+    // 3) сортировка массива чисел без использования Arrays.sort(); Сортировка Пузырьком
+    public static int[] sort2(int[] array) {
+        int temp=0;
+        for (int i = array.length-1; i >0 ; i--) {
+            for (int j = 0; j <i ; j++) {
+                if(array[j]>array[j+1]){
+                    temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                }
+            }
+        }
+        return array;
+    }
+    // перевенуть каждое слово в строке сохраняя порядок слов
+    public static String reverse(String phrase){
+        return phrase;
+    }
+}
+
