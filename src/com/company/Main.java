0911102ea49a3ac01d3df06832856dc2098614a7
AnzhelikaK <AnzhelikaShch11@gmail.com>
2020-04-05 21:48:33 +0300

    добавила реверс слов в строчке

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("перевернуть массив чисел в обратном порядке");
         System.out.println(Arrays.toString(Reverse.reverse2(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})));
-        int [] a=Reverse.reverse(null);
+        int [] b=null;
+        int [] a=Reverse.reverse(b);
 
         System.out.println("\n перевернуть строку в обратном порядке");
-       String b=Reverse.reverseString("Anzhelika");
+       String c=Reverse.reverseString("Anzhelika");
         System.out.println("Result: "+b+"\n");
 
         System.out.println("сортировка массива чисел без использования Arrays.sort() - Cортировка Выбором");
@@ -17,5 +18,8 @@ public class Main {
 
         System.out.println("\n сортировка массива чисел без использования Arrays.sort() - Cортировка Пузырьком");
         System.out.println(Arrays.toString(Reverse.sort2(new int[] {1,0,7,9,5,1,4,2,2,7,6,0,2,1,-5,-8,-97})));
+
+        System.out.println("перевенуть каждое слово в строке сохраняя порядок слов");
+        System.out.println(Reverse.reverse("Anzhelika Kryvapust     Shcharbitskaya"));
     }
 }
--- src/com/company/Reverse.java
+++ src/com/company/Reverse.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import javax.crypto.NullCipher;
+import java.util.ArrayList;
 
 public class Reverse {
     // 1) перевернуть массив чисел в обратном порядке
@@ -61,23 +62,37 @@ public class Reverse {
         }
         return array;
     }
+
     // 3) сортировка массива чисел без использования Arrays.sort(); Сортировка Пузырьком
     public static int[] sort2(int[] array) {
-        int temp=0;
-        for (int i = array.length-1; i >0 ; i--) {
-            for (int j = 0; j <i ; j++) {
-                if(array[j]>array[j+1]){
-                    temp=array[j];
-                    array[j]=array[j+1];
-                    array[j+1]=temp;
+        int temp = 0;
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
         return array;
     }
+
     // перевенуть каждое слово в строке сохраняя порядок слов
-    public static String reverse(String phrase){
-        return phrase;
+    public static String reverse(String phrase) {
+        String delimitr = " ";
+        String[] arrayOfphrase;
+        StringBuilder phraseBuilt = new StringBuilder();
+        arrayOfphrase = phrase.split(delimitr);
+        for (int i = 0; i < arrayOfphrase.length; i++) {
+            if (arrayOfphrase[i].equals(delimitr)) {
+            } else {
+                StringBuilder temp = new StringBuilder(arrayOfphrase[i]);
+                phraseBuilt.append(temp.reverse().toString()+" ");
+
+            }
+        }
+        return phraseBuilt.toString();
     }
 }
 
