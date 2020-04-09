e0117fd5ca076042dbd936c2d5e88ad755aa3d64
AnzhelikaK <AnzhelikaShch11@gmail.com>
2020-04-09 22:39:50 +0300

    Change decor+

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,26 +4,36 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("перевернуть массив чисел в обратном порядке");
-        System.out.println(Arrays.toString(Reverse.reverse2(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})));
+        // 1.1
+        System.out.println("\n1.1 Reverse the array of numbers vise-versa\n");
+        System.out.println(Arrays.toString(Reverse.reverse(new int[]{11, 13, 14, 15, 16, 17, 18, 19, 20})));
         int[] b = null;
-        int[] a = Reverse.reverse(b);
+        System.out.println(Arrays.toString(Reverse.reverse(b)));
 
-        System.out.println("\n 2.1 перевернуть строку в обратном порядке");
+        // 1.2
+        System.out.println("\n1.2 Reverse the array of numbers vise-versa: the way 2 - reverse without an additional array\n");
+        System.out.println(Arrays.toString(Reverse.reverse2(new int[]{11, 13, 14, 15, 16, 17, 18, 19, 20})));
+        System.out.println(Arrays.toString(Reverse.reverse2(b)));
+
+        // 2.1
+        System.out.println("\n2.1 Flip the line-String vice-versa\n");
         String c = Reverse.reverseString("Anzhelika");
         System.out.println("Result: " + c + "\n");
 
-        System.out.println("\n 2.2 flip the line vice-versa without StringBuilder");
+        // 2.2
+        System.out.println("\n2.2 Flip the line vice-versa without StringBuilder");
         System.out.println(Reverse.reversString2("Anzhelika"));
 
-        System.out.println("сортировка массива чисел без использования Arrays.sort() - Cортировка Выбором");
+        // 3.1
+        System.out.println("\n3.1 Sort the array of numbers without Arrays.sort(array). Selection sort (RU: Сортировка выбором)\n");
         System.out.println(Arrays.toString(Reverse.sort(new int[]{1, 0, 7, 9, 5, 1, 4, 2, 2, 7, 6, 0, 2, 1, -5, -8, -97})));
 
-        System.out.println("\n сортировка массива чисел без использования Arrays.sort() - Cортировка Пузырьком");
+        // 3.2
+        System.out.println("\n3.2 Sort the array of numbers without Arrays.sort(array). Bubble sort (RU: Сортировка Пузырьком)\n");
         System.out.println(Arrays.toString(Reverse.sort2(new int[]{1, 0, 7, 9, 5, 1, 4, 2, 2, 7, 6, 0, 2, 1, -5, -8, -97})));
 
-        System.out.println("перевенуть каждое слово в строке сохраняя порядок слов");
-        System.out.println(Reverse.reverse("Anzhelika Kryvapust     Shcharbitskaya"));
-        String n = new String();
-    }
+        // 4.
+        System.out.println("\n4. Reverse each word in the line save the order of words\n");
+        System.out.println(Reverse.reverse(" Anzhelika Kryvapust     Shcharbitskaya "));
+            }
 }
--- src/com/company/Reverse.java
+++ src/com/company/Reverse.java
@@ -4,21 +4,21 @@ import javax.crypto.NullCipher;
 import java.util.ArrayList;
 
 public class Reverse {
-    // 1.1 перевернуть массив чисел в обратном порядке
+    // 1.1 Reverse the array of numbers vise-versa
     public static int[] reverse(int[] array) {
-        if (array == null) {
+        if (array == null) {                                        // All time CHECK on null
             System.out.println("Received empty array");
-            return null;
+            return null;                                             // first option
+            // return throw new NullPointerException("You sent null");  // second option
+            // return new int []{};                                     // third option
         } else {
             int[] arrayTemporary = new int[array.length];
-            for (int i = 0; i < array.length; i++) {
-                arrayTemporary[i] = array.length - i;
-            }
+            for (int i = 0; i < array.length; i++) arrayTemporary[i] = array[array.length - i - 1];
             return arrayTemporary;
         }
     }
 
-    // 1.2 перевернуть массив чисел в обратном порядке: способ 2 - переворачивание без дополниетлнього массива
+    // 1.2 Reverse the array of numbers vise-versa: the way 2 - reverse without an additional array
     public static int[] reverse2(int[] array) {
         if (array != null) {
             int x = 0;
@@ -28,12 +28,11 @@ public class Reverse {
                 array[array.length - 1 - i] = x;
             }
             return array;
-        } else return new int[]{0};      //  pay attention
+        } else return new int[]{};      //  pay attention (how write else + return - IN ONE LINE
 
     }
 
-
-    // 2.1 flip the line vice-versa
+    // 2.1 flip the line-String vice-versa
     public static String reverseString(String str) {
         StringBuilder stringBuilder = new StringBuilder();
         if (str != null) {
@@ -42,40 +41,44 @@ public class Reverse {
                 stringBuilder.append(charArray[i]);
             }
         }
-            return stringBuilder.toString();
+        return stringBuilder.toString();
     }
 
     // 2.2 flip the line vice-versa without StringBuilder
-    public static String reversString2(String str){
-        int l=str.length();
-        char [] arrayOfStr=new char[l];
-        for (int i = 0; i <l ; i++) {
-            arrayOfStr[i]=str.charAt(l-i-1);
-        }
-        return new String(arrayOfStr);
+    public static String reversString2(String str) {
+        if (str != null) {
+        int l = str.length();
+        char[] arrayOfStr = new char[l];
+
+            for (int i = l - 1; i >= 0; i--) {
+                arrayOfStr[l - i - 1] = str.charAt(i);
+            }
+
+        return String.valueOf(arrayOfStr);
+        // return new String(arrayOfStr);
+        } else return null;
     }
 
-    // 3.1 сортировка массива чисел без использования Arrays.sort(); Сортировка выбором
+    // 3.1 Sort the array of numbers without Arrays.sort(array). Selection sort (RU: Сортировка выбором)
     public static int[] sort(int[] array) {
-
-        for (int i = 0; i < array.length-1; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             int min = array[i];
-            int i_min = i;
+            int iMin = i;
             for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < min) {
                     min = array[j];
-                    i_min = j;
+                    iMin = j;
                 }
             }
-            if (array[i] != min) {  // можно также по индеку сравнивать i!=i_min
-                array[i_min] = array[i];
+            if (array[i] != min) {  // or compare with use INDEX:  i!=iMin
+                array[iMin] = array[i];
                 array[i] = min;
             }
         }
         return array;
     }
 
-    // 3.2 сортировка массива чисел без использования Arrays.sort(); Сортировка Пузырьком
+    // 3.2 Sort the array of numbers without Arrays.sort(array). Bubble sort (RU: Сортировка Пузырьком)
     public static int[] sort2(int[] array) {
         int temp = 0;
         for (int i = array.length - 1; i > 0; i--) {
@@ -89,21 +92,23 @@ public class Reverse {
         }
         return array;
     }
-    // 4 перевенуть каждое слово в строке сохраняя порядок слов
+
+    // 4. Reverse each word in the line save the order of words
     public static String reverse(String phrase) {
         String delimitr = " ";
-        String[] arrayOfphrase;
+        String[] arrayOfPhrase;
         StringBuilder phraseBuilt = new StringBuilder();
-        arrayOfphrase = phrase.split(delimitr);
-        for (int i = 0; i < arrayOfphrase.length; i++) {
-            if (arrayOfphrase[i].equals(delimitr)) {
-            } else {
-                StringBuilder temp = new StringBuilder(arrayOfphrase[i]);
-                phraseBuilt.append(temp.reverse().toString() + " ");
-
+        arrayOfPhrase = phrase.split(delimitr);
+        for (int i = 0; i < arrayOfPhrase.length; i++) {
+            if (!arrayOfPhrase[i].equals(delimitr)) {
+                StringBuilder temp = new StringBuilder(arrayOfPhrase[i]);
+                phraseBuilt.append(temp.reverse().toString()).append(" ");
             }
         }
-        return phraseBuilt.toString();
+        //  return phraseBuilt.toString().trim(); // to cut " " at the end, but then cut also " "  at the beginning
+        // or
+        String a = phraseBuilt.toString();
+        return a.substring(0, a.length() - 1);
     }
 }
 
